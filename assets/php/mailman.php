573a0834ebe8f15a5451e727795dd1bd5202bfc4
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-04-01 19:41:12 +0300

    Little Update
    Signed-off-by: Halil İbrahim Direktör <hidirektor@gmail.com>

--- assets/php/mailman.php
+++ assets/php/mailman.php
@@ -1,43 +1,65 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\PHPMailer;
 
 require 'PHPMailer-master/src/Exception.php';
 require 'PHPMailer-master/src/SMTP.php';
 require 'PHPMailer-master/src/PHPMailer.php';
 
-$success = false;
-$userName = isset( $_POST['name'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['name'] ) : "";
-$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $to_email = "hidirektor@gmail.com";
+  $subject = "Talentia48h Yeni Başvuru";
 
-$mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8';
+  // Sanitize input data
+  $user_name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+  $user_email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
-try {
-  //$mail->SMTPDebug = SMTP::DEBUG_OFF;SMTP::DEBUG_SERVER
-  $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-  $mail->isSMTP();
-  $mail->Host       = 'mail.hidirektor.com.tr';
-  $mail->SMTPAuth   = true;
-  $mail->Username   = 'noreply@hidirektor.com.tr';
-  $mail->Password   = '&#4?{oKWoOM0';
-  $mail->SMTPSecure = 'ssl';
-  $mail->Port       = 465;
+  // Validate input data
+  if (strlen($user_name) < 2) {
+    $output = json_encode(array('type' => 'error', 'text' => 'Girilen isim soyisim çok kısa!'));
+    die($output);
+  }
+  if (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
+    $output = json_encode(array('type' => 'error', 'text' => 'Lütfen geçerli bir E-Posta adresi gir!'));
+    die($output);
+  }
 
-  //Recipients
-  $mail->setFrom('noreply@hidirektor.com.tr', 'hidirektor.com.tr');
-  $mail->addAddress('hidirektor@gmail.com');
+  // Email body
+  $message_body = $message . "\r\n\r\n-" . $user_name . "\r\nEmail: " . $user_email . "\r\n";
 
-  // Content
-  $mail->isHTML(false);
-  $mail->Subject = "Talentia48h -- Yeni Başvuru";
-  $mail->Body = "İsim: " . $userName . "\nE-Posta: " . $senderEmail . "\nMesaj: " . $message;
+  // Send email using PHPMailer
+  $mail = new PHPMailer(true);
+  $mail->CharSet = 'UTF-8';
 
-  $mail->send();
-  echo "<script>window.location = 'index.html?success=1';</script>";
-} catch (Exception $e) {
-  echo "<script>window.location = 'index.html?error=1';</script>";
+  try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'mail.hidirektor.com.tr';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'noreply@hidirektor.com.tr';
+    $mail->Password   = '&#4?{oKWoOM0';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+
+    //Recipients
+    $mail->setFrom($user_email, $user_name);
+    $mail->addAddress($to_email);
+
+    //Content
+    $mail->isHTML(false);
+    $mail->Subject = $subject;
+    $mail->Body    = $message_body;
+
+    $mail->send();
+    $output = json_encode(array('type' => 'message', 'text' => $user_name . ', başvurun için teşekkür ederiz :))'));
+    die($output);
+  } catch (Exception $e) {
+    $output = json_encode(array('type' => 'error', 'text' => 'Başvurunuz alınamadı. Mail hatası: ' . $mail->ErrorInfo));
+    die($output);
+  }
 }
 ?>
